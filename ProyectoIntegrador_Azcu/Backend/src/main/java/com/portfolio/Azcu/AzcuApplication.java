da6cf77fa86a2ac490175f0b127e6e78dca5b49d
Sebazcu <94652657+Sebazcu@users.noreply.github.com>
2022-06-23 11:36:56 -0300

    cree el Backend

---
+++ ProyectoIntegrador_Azcu/Backend/src/main/java/com/portfolio/Azcu/AzcuApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Azcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzcuApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_Azcu/Backend/src/test/java/com/portfolio/Azcu/AzcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Azcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
